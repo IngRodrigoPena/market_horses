57d5ab1deaefac6c0abeaa50cfc0f6d8ef5446f6
IngRodrigoPena <ingrodrigopena@gmail.com>
2026-02-09 20:13:40 -0600

    configuracion Inicial

---
+++ src/main/java/com/nocountry/markethorses/MarketHorsesApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.markethorses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketHorsesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketHorsesApplication.class, args);
+	}
+
+}
